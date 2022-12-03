95c4009a1e1149a718015b08679a93e5838a4d50
kaaveh <kaaveh@pm.me>
2022-12-03 09:07:56 +0330

    [solution] Day03

---
+++ src/Day03.kt
@@ -0,0 +1,74 @@
+fun main() {
+    fun part1(input: List<String>): Int {
+        val priorityScore = mutableListOf<Int>()
+
+        input.forEach { items ->
+            val first = items.substring(0 until items.length / 2)
+            val second = items.substring(items.length / 2 until items.length)
+
+            var common = 'a'
+            for (i in first.indices) {
+                for (j in second.indices) {
+                    if (first[i] == second[j])
+                        common = first[i]
+                }
+            }
+
+            if (common.isLowerCase()) {
+                priorityScore.add(
+                    common - 'a' + 1
+                )
+            } else {
+                priorityScore.add(
+                    common - 'A' + 27
+                )
+            }
+        }
+
+        return priorityScore.sum()
+    }
+
+    fun part2(input: List<String>): Int {
+
+        val priorityScore = mutableListOf<Int>()
+
+        for (x in 0 until input.size / 3) {
+            val first = input[3 * x]
+            val second = input[3 * x + 1]
+            val third = input[3 * x + 2]
+
+            var common = 'a'
+            for (i in first.indices) {
+                for (j in second.indices) {
+                    for (k in third.indices) {
+                        if (first[i] == second[j])
+                            if (first[i] == third[k])
+                                common = first[i]
+                    }
+                }
+            }
+
+            if (common.isLowerCase()) {
+                priorityScore.add(
+                    common - 'a' + 1
+                )
+            } else {
+                priorityScore.add(
+                    common - 'A' + 27
+                )
+            }
+
+
+        }
+
+        return priorityScore.sum()
+    }
+
+    val testInput = readInput("Day03_test")
+    check(part1(testInput) == 157)
+    check(part2(testInput) == 70)
+
+    val input = readInput("Day03")
+    println(part1(input))
+    println(part2(input))
+}
