6466161d92d44d3c8c628ed1b4333146dc68353b
kaaveh <kaaveh@pm.me>
2022-12-04 18:47:51 +0330

    [improve solution] Day04

--- src/Day04.kt
+++ src/Day04.kt
@@ -1,54 +1,32 @@
+infix fun IntRange.subrange(other: IntRange): Boolean =
+    (this.first in other && this.last in other) || other.first in this && other.last in this
+
+private fun parse(row: String): Pair<IntRange, IntRange> {
+    val (section1Start, section1End) = row.substringBefore(",").split("-").map { it.toInt() }
+    val (section2Start, section2End) = row.substringAfter(",").split("-").map { it.toInt() }
+    return section1Start..section1End to section2Start..section2End
+}
+
+private fun part1(input: List<String>): Long = input.sumOf { row ->
+    val (section1, section2) = parse(row = row)
+    when {
+        section1 subrange section2 -> 1L
+        else -> 0L
+    }
+}
+
+private fun part2(input: List<String>): Long = input.sumOf { row ->
+    val (section1, section2) = parse(row = row)
+    when {
+        (section1 intersect section2).isNotEmpty() -> 1
+        else -> 0L
+    }
+}
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        var common = 0
-
-        input.forEach { row ->
-            val (section1, section2) = row.split(",")
-
-            val (section1First, section1Second) = section1.split("-").map { it.toInt() }
-            val (section2First, section2Second) = section2.split("-").map { it.toInt() }
-
-            common += when {
-                (section1First <= section2First && section1Second >= section2Second) ||
-                        section2First <= section1First && section2Second >= section1Second
-                -> 1
-
-                else -> 0
-            }
-
-        }
-
-        return common
-    }
-
-    fun part2(input: List<String>): Int {
-        var common = 0
-
-        input.forEach { row ->
-            val (section1, section2) = row.split(",")
-
-            val (section1Start, section1End) = section1.split("-").map { it.toInt() }
-            val (section2Start, section2End) = section2.split("-").map { it.toInt() }
-
-            common += when {
-                (section1Start <= section2Start && section1End >= section2End) ||
-                        (section2Start <= section1Start && section2End >= section1End) ||
-                        section1Start in section2Start..section2End ||
-                        section2Start in section1Start..section1End ||
-                        section2End in section1Start..section1End
-                -> 1
-
-                else -> 0
-            }
-
-        }
-
-        return common
-    }
-
     val testInput = readInput("Day04_test")
-    check(part1(testInput) == 2)
-    check(part2(testInput) == 4)
+    check(part1(testInput) == 2L)
+    check(part2(testInput) == 4L)
 
     val input = readInput("Day04")
     println(part1(input))
