dc782a4f8c4fe27b483077de272b5a3d8b4ca0a1
kaaveh <kaaveh@pm.me>
2022-12-04 13:18:46 +0330

    [Improve Solution] Day03

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,27 +1,27 @@
 val Char.priority
-    get(): Int {
-        return if (this.isLowerCase()) {
-            this - 'a' + 1
-
-        } else {
-            this - 'A' + 27
-        }
+    get(): Int = when (this) {
+        in 'a'..'z' -> this - 'a' + 1
+        in 'A'..'Z' -> this - 'A' + 27
+        else -> error("Wrong input! $this")
     }
+
+inline infix fun String.singleIntersect(other: String) = (toSet() intersect other.toSet()).single()
+inline infix fun Char.singleIntersect(other: String) = (setOf(this) intersect other.toSet()).single()
+
+fun part1(input: List<String>): Int = input.sumOf { items ->
+    val first = items.substring(0 until items.length / 2)
+    val second = items.substring(items.length / 2)
+    val common = first singleIntersect second
+    common.priority
+}
+
+fun part2(input: List<String>): Int = input.chunked(3).sumOf { elfGroup ->
+    val (first, second, third) = elfGroup
+    val common = first singleIntersect second singleIntersect third
+    common.priority
+}
 
 fun main() {
-
-    fun part1(input: List<String>): Int = input.sumOf { items ->
-        val first = items.substring(0 until items.length / 2)
-        val second = items.substring(items.length / 2)
-        val common = first.first { it in second }
-        common.priority
-    }
-
-    fun part2(input: List<String>): Int = input.chunked(3).sumOf { elfGroup ->
-        val common = elfGroup[0].first { it in elfGroup[1] && it in elfGroup[2] }
-        common.priority
-    }
-
     val testInput = readInput("Day03_test")
     check(part1(testInput) == 157)
     check(part2(testInput) == 70)
