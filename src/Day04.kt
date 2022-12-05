459910e16416718b0bc4442faee7bfce7a40ea39
kaaveh <kaaveh@pm.me>
2022-12-05 07:45:54 +0330

    [improve solution] Day04

--- src/Day04.kt
+++ src/Day04.kt
@@ -1,5 +1,5 @@
-infix fun IntRange.subrange(other: IntRange): Boolean =
-    (this.first in other && this.last in other) || other.first in this && other.last in this
+infix fun IntRange.fullyOverlap(other: IntRange): Boolean =
+    (first in other && last in other) || other.first in this && other.last in this
 
 private fun parse(row: String): Pair<IntRange, IntRange> {
     val (section1Start, section1End) = row.substringBefore(",").split("-").map { it.toInt() }
@@ -7,26 +7,20 @@ private fun parse(row: String): Pair<IntRange, IntRange> {
     return section1Start..section1End to section2Start..section2End
 }
 
-private fun part1(input: List<String>): Long = input.sumOf { row ->
+private fun part1(input: List<String>): Int = input.count { row ->
     val (section1, section2) = parse(row = row)
-    when {
-        section1 subrange section2 -> 1L
-        else -> 0L
-    }
+    section1 fullyOverlap section2
 }
 
-private fun part2(input: List<String>): Long = input.sumOf { row ->
+private fun part2(input: List<String>): Int = input.count { row ->
     val (section1, section2) = parse(row = row)
-    when {
-        (section1 intersect section2).isNotEmpty() -> 1
-        else -> 0L
-    }
+    section1.any { it in section2 }
 }
 
 fun main() {
     val testInput = readInput("Day04_test")
-    check(part1(testInput) == 2L)
-    check(part2(testInput) == 4L)
+    check(part1(testInput) == 2)
+    check(part2(testInput) == 4)
 
     val input = readInput("Day04")
     println(part1(input))
