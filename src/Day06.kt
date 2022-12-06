104c8c98925c1f0d3dae23ff85bebdc3f57bb745
kaaveh <kaaveh@pm.me>
2022-12-06 12:37:07 +0330

    [improve solution] Day06

--- src/Day06.kt
+++ src/Day06.kt
@@ -1,35 +1,11 @@
-private fun part1(input: String): Int {
-    for (i in input.indices) {
-        val count = input.substring(i, i + 4)
-            .toSet()
-            .size
-        if(count == 4){
-            return i + 4
-        }
-    }
-
-    return 0
-}
-
-private fun part2(input:String): Int {
-    for (i in input.indices) {
-        val count = input.substring(i, i + 14)
-            .toSet()
-            .size
-        if(count == 14){
-            return i + 14
-        }
-    }
-
-    return 0
-}
+private fun part1And2(input: String, size: Int) = input.windowed(size).indexOfFirst { it.toSet().size == size } + size
 
 fun main() {
     val testInput = readInput("Day06_test")[0]
-    check(part1(testInput) == 7)
-    check(part2(testInput) == 19)
+    check(part1And2(testInput, 4) == 7)
+    check(part1And2(testInput, 14) == 19)
 
     val input = readInput("Day06")[0]
-    println(part1(input))
-    println(part2(input))
+    println(part1And2(input, 4))
+    println(part1And2(input, 14))
 }
