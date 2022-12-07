fc9833f632cacd4be3e584d0109ee6eff0401379
kaaveh <kaaveh@pm.me>
2022-12-07 18:44:42 +0330

    [improve solution] Day06

--- src/Day06.kt
+++ src/Day06.kt
@@ -1,4 +1,9 @@
-private fun part1And2(input: String, size: Int) = input.windowed(size).indexOfFirst { it.toSet().size == size } + size
+private fun part1And2(input: String, size: Int) = input.windowed(size).indexOfFirst { it.allUnique() } + size
+
+fun String.allUnique(): Boolean {
+    val s = hashSetOf<Char>()
+    return all { s.add(it) }
+}
 
 fun main() {
     val testInput = readInput("Day06_test")[0]
