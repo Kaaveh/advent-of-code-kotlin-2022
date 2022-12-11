ffafa9fdac517411f0333fadda8cea2c38f19d17
kaaveh <kaaveh@pm.me>
2022-12-11 07:42:27 +0330

    [improve solution] Day08

--- src/Day08.kt
+++ src/Day08.kt
@@ -1,100 +1,62 @@
-private fun part1(input: List<String>): Int {
-    var total = 0
-//    val size = 7
-    val size = 101
+private fun parse(input: List<String>, size: Int): MutableList<MutableList<Int>> {
     val array = MutableList(size) { MutableList(size) { 0 } }
 
     input.forEachIndexed { indexX: Int, row: String ->
         row.forEachIndexed { indexY, element ->
-            array[indexX + 1][indexY + 1] = element.toString().toInt()
+            array[indexX][indexY] = element.digitToInt()
         }
     }
+    return array
+}
 
-    for (i in 2..size - 3) {
-        for (j in 2..size - 3) {
+private fun part1(input: List<String>): Int {
+    var total = 0
+    val size = input.size
+    val array = parse(input, size)
+
+    for (i in 0 until size) {
+        for (j in 0 until size) {
             val element = array[i][j]
-            val maxRowBefore = array[i].subList(fromIndex = 0, toIndex = j).max()
-            val maxRowAfter = array[i].subList(fromIndex = j + 1, toIndex = array[i].size).max()
-            val maxColumnBefore = true.let {
-                val tmp = mutableListOf<Int>()
-
-                for (k in 0 until i) {
-                    tmp.add(
-                        array[k][j]
-                    )
-                }
-                tmp
-                tmp.max()
-            }
-            val maxColumnAfter = true.let {
-                val tmp = mutableListOf<Int>()
-
-                for (k in i + 1 until array[i].size) {
-                    tmp.add(
-                        array[k][j]
-                    )
-                }
-                tmp
-                tmp.max()
-
-            }
+            val leftVisible = (0 until j).all { array[i][it] < element }
+            val rightVisible = (j + 1 until array[i].size).all { array[i][it] < element }
+            val topVisible = (0 until i).all { k -> array[k][j] < element }
+            val bottomVisible = (i + 1 until array[i].size).all { k -> array[k][j] < element }
             if (
-                element <= maxRowBefore &&
-                element <= maxRowAfter &&
-                element <= maxColumnBefore &&
-                element <= maxColumnAfter
+                leftVisible || rightVisible || topVisible || bottomVisible ||
+                i == 0 || j == 0 || i == size - 1 || j == size - 1
             ) {
                 total++
             }
         }
     }
 
-    return (size - 2) * (size - 2) - total
+    return total
 }
 
 private fun part2(input: List<String>): Int {
     var max = 0
-
-
-//    val size = 5
-    val size = 99
-    val array = MutableList(size) { MutableList(size) { 0 } }
-
-    input.forEachIndexed { indexX: Int, row: String ->
-        row.forEachIndexed { indexY, element ->
-            array[indexX][indexY] = element.toString().toInt()
-        }
-    }
+    val size = input.size
+    val array = parse(input, size)
 
     for (i in 1 until size - 1) {
         for (j in 1 until size - 1) {
             val element = array[i][j]
             val scoreRowBefore = array[i].subList(fromIndex = 0, toIndex = j).reversed().let {
-                var index = 0
-
+                var index = it.size
                 for (k in it.indices) {
-                    if (it[k] < element) {
-                        index++
-                    } else if (it[k] >= element) {
-                        index++
-                        break
-                    } else {
+                    if (it[k] >= element){
+                        index = k + 1
                         break
                     }
                 }
-
                 index
             }
             val scoreRowAfter = array[i].subList(fromIndex = j + 1, toIndex = array[i].size).let {
-                var index = 0
+                var index = it.size
 
                 for (k in it.indices) {
-                    if (it[k] < element) {
-                        index++
-                    } else if (it[k] >= element) {
-                        index++
-                        break
-                    } else {
+                    if (it[k] >= element){
+                        index = k + 1
                         break
                     }
                 }
@@ -102,10 +64,10 @@ private fun part2(input: List<String>): Int {
                 index
             }
 
-            val scoreColumnBefore = true.let {
+            val scoreColumnBefore = (i - 1 downTo 0).let {
                 var index = 0
 
-                for (k in i-1 downTo 0) {
+                for (k in i - 1 downTo 0) {
                     if (array[k][j] < element) {
                         index++
                     } else if (array[k][j] >= element) {
@@ -145,10 +107,10 @@ private fun part2(input: List<String>): Int {
 
 fun main() {
     val testInput = readInput("Day08_test")
-//    check(part1(testInput) == 21)
-//    check(part2(testInput) == 8)
+    check(part1(testInput) == 21)
+    check(part2(testInput) == 8)
 
     val input = readInput("Day08")
 //    println(part1(input))
-    println(part2(input))
+//    println(part2(input))
 }
